5370eb715d0d9d38addb0c5602405532e76959f3
kalaider <a.kalaider@yandex.ru>
2017-10-05 18:00:13 +0300

    Viewport mapper moved to viewport.h

--- include/util/common/plot/viewporter.h
+++ include/util/common/plot/viewporter.h
@@ -9,45 +9,6 @@
 namespace plot
 {
 
-    // viewport mapper
-
-
-
-
-    using viewport_mapper_t = std::function < viewport (const viewport &) > ;
-
-
-
-    static inline viewport_mapper_t create_viewport_mapper(const viewport & vp)
-    {
-        return [vp] (const viewport &) { return vp; };
-    }
-
-    static inline viewport_mapper_t create_viewport_mapper(const world_t & w)
-    {
-        return [w] (const viewport & vp) { return viewport(vp.screen, w); };
-    }
-
-    static inline viewport_mapper_t create_viewport_mapper(const screen_t & s)
-    {
-        return [s] (const viewport & vp) { return viewport(s, vp.world); };
-    }
-
-    static inline viewport_mapper_t create_viewport_mapper(std::shared_ptr < viewport > vp)
-    {
-        return [vp] (const viewport &) { return *vp; };
-    }
-
-    static inline viewport_mapper_t create_viewport_mapper(std::shared_ptr < world_t > w)
-    {
-        return [w] (const viewport & vp) { return viewport(vp.screen, *w); };
-    }
-
-    static inline viewport_mapper_t create_viewport_mapper(std::shared_ptr < screen_t > s)
-    {
-        return [s] (const viewport & vp) { return viewport(*s, vp.world); };
-    }
-
 
 
 
