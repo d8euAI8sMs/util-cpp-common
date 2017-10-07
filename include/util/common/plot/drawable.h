03e32104596714dbe8b5a8374ffec8463bda8b32
kalaider <a.kalaider@yandex.ru>
2017-10-07 11:40:13 +0300

    Added a standard way to draw drawable at a point

--- include/util/common/plot/drawable.h
+++ include/util/common/plot/drawable.h
@@ -6,6 +6,7 @@
 #include <functional>
 
 #include <util/common/plot/viewport.h>
+#include <util/common/plot/shape.h>
 
 namespace plot
 {
@@ -16,6 +17,14 @@ namespace plot
     public:
 
         virtual void draw(CDC &dc, const viewport &bounds) = 0;
+
+        virtual void draw_at(CDC &dc, const viewport &bounds, const point < double > origin)
+        {
+            point < int > origin_at_screen = bounds.world_to_screen().xy(origin);
+            dc.OffsetViewportOrg(origin_at_screen.x, origin_at_screen.y);
+            draw(dc, bounds);
+            dc.OffsetViewportOrg(-origin_at_screen.x, -origin_at_screen.y);
+        }
 
         virtual ~drawable()
         {
