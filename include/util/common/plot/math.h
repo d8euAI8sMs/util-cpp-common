e0b71aa6b93aa49ea1ef6922c112339d4c7604d1
kalaider <a.kalaider@yandex.ru>
2017-10-09 01:32:05 +0300

    A bridge between math and plot subpackage added

---
+++ include/util/common/plot/math.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <util/common/plot/plot.h>
+#include <util/common/math/common.h>
+
+namespace plot
+{
+
+    template < typename _points_t >
+    static inline void sampled_t_to_data(common::sampled_t sampled,
+                                         _points_t & points,
+                                         double origin = 0,
+                                         common::un_op_t mapper = common::identity_un_op())
+    {
+        points.clear();
+        for (size_t i = 0; i < sampled.count; ++i)
+        {
+            points.emplace_back(origin + i * sampled.period, mapper(i, sampled.samples[i]));
+        }
+    }
+}
