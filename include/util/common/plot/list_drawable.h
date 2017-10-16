50c07c79a79356a3e9f9f2528e1910b3b3fd1e03
kalaider <a.kalaider@yandex.ru>
2017-10-16 23:41:52 +0300

    list_drawable migrated to new data mapper

--- include/util/common/plot/list_drawable.h
+++ include/util/common/plot/list_drawable.h
@@ -3,6 +3,7 @@
 #include <utility>
 
 #include <util/common/ptr.h>
+#include <util/common/iterable.h>
 #include <util/common/plot/palette.h>
 #include <util/common/plot/drawable.h>
 #include <util/common/plot/data_source.h>
@@ -29,7 +30,7 @@ namespace plot
     public:
 
         data_source_t < _container_t > data_source;
-        data_mapper_t < typename _container_t::iterator, _mapped_t > data_mapper;
+        util::data_mapper_t < _container_t, typename _container_t::const_iterator, _mapped_t > data_mapper;
         drawable::ptr_t  point_painter;
         palette::pen_ptr line_pen;
 
@@ -59,36 +60,20 @@ namespace plot
 
         virtual void do_draw(CDC &dc, const viewport &bounds) override
         {
-            if (!data_source) return;
+            if ((!data_source) || (!data_mapper) && (!util::is_identity_mappable < typename _container_t::const_iterator, _mapped_t > ())) return;
             auto data = data_source(bounds);
             if (data->empty()) return;
 
             point < double > p;
 
-            if (data_mapper)
-            {
-                p = data_mapper(data->begin(), 0);
-            }
-            else
-            {
-                p = *data->begin();
-            }
-
-            dc.MoveTo(bounds.world_to_screen().xy(p));
+            dc.MoveTo(bounds.world_to_screen().xy(util::get_mapped_value_or_default < _container_t, typename _container_t::const_iterator, _mapped_t > (data_mapper, *data, data->begin(), 0)));
 
             size_t idx = 0;
             auto end = data->end();
 
             for (auto it = data->begin(); it != end; ++it, ++idx)
             {
-                if (data_mapper)
-                {
-                    p = data_mapper(it, idx);
-                }
-                else
-                {
-                    p = *it;
-                }
+                p = util::get_mapped_value_or_default < _container_t, typename _container_t::const_iterator, _mapped_t > (data_mapper, *data, it, idx);
                 if (line_pen)
                 {
                     auto _old = dc.SelectObject(*line_pen);
