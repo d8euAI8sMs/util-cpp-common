4ee2109137d6857091eab1cd4e230899fa967efa
kalaider <a.kalaider@yandex.ru>
2017-10-18 19:58:08 +0300

    Additional types and functions added

--- include/util/common/math/dsolve.h
+++ include/util/common/math/dsolve.h
@@ -7,7 +7,15 @@
 namespace math
 {
 
+    using dfunc3_t = std::function < v3 (double t, const v3 &x) >;
+
     using dfunc3s_t = std::function < v3 (double t, const v3 &x, const v3 &dx) >;
+
+    struct dresult3
+    {
+        double t;
+        v3 x;
+    };
 
     struct dresult3s
     {
@@ -19,6 +27,16 @@ namespace math
     {
         v3 k1, k2, k3, k4;
     };
+
+    inline rk4_coefs3 _get_rk4_coefs3(const dfunc3_t &fn, double t, double dt, const v3 &x)
+    {
+        rk4_coefs3 c;
+        c.k1 = fn(t, x) * dt;
+        c.k2 = fn(t + dt / 2, x + c.k1 / 2) * dt;
+        c.k3 = fn(t + dt / 2, x + c.k2 / 2) * dt;
+        c.k4 = fn(t + dt, x + c.k3) * dt;
+        return c;
+    }
 
     inline rk4_coefs3 _get_rk4_coefs3s(const dfunc3s_t &fn, double t, double dt, const v3 &x, const v3 &dx)
     {
@@ -45,6 +63,22 @@ namespace math
             t + dt,
             x + dx * dt + (c.k1 + c.k2 + c.k3) / 6 * dt,
             dx + (c.k1 + 2 * c.k2 + 2 * c.k3 + c.k4) / 6
+        };
+    }
+
+    // solves the passed vector differential equation
+    // using Runge-Kutta (4) method
+    // fn - the vector function
+    // t  - the scalar parameter (time)
+    // dt - the scalar parameter step (time delta)
+    // x  - the initial condition
+    inline dresult3 rk4_solve3(dfunc3s_t fn, double t, double dt, const v3 &x)
+    {
+        rk4_coefs3 c = _get_rk4_coefs3(fn, t, dt, x);
+        return
+        {
+            t + dt,
+            x + (c.k1 + 2 * c.k2 + 2 * c.k3 + c.k4) / 6
         };
     }
 }
