c752c33ebdcec6e38cc1e2df5a242cef38d16145
kalaider <a.kalaider@yandex.ru>
2017-10-19 20:18:00 +0300

    dsolve templatized

--- include/util/common/math/dsolve.h
+++ include/util/common/math/dsolve.h
@@ -7,30 +7,36 @@
 namespace math
 {
 
-    using dfunc3_t = std::function < v3 (double t, const v3 &x) >;
+    template < typename _v3 = v3 < > >
+    using dfunc3_t = std::function < _v3 (double t, const _v3 &x) >;
 
-    using dfunc3s_t = std::function < v3 (double t, const v3 &x, const v3 &dx) >;
+    template < typename _v3 = v3 < > >
+    using dfunc3s_t = std::function < _v3 (double t, const _v3 &x, const _v3 &dx) >;
 
+    template < typename _v3 = v3 < > >
     struct dresult3
     {
         double t;
-        v3 x;
+        _v3 x;
     };
 
+    template < typename _v3 = v3 < > >
     struct dresult3s
     {
         double t;
-        v3 x, dx;
+        _v3 x, dx;
     };
 
+    template < typename _v3 = v3 < > >
     struct rk4_coefs3
     {
-        v3 k1, k2, k3, k4;
+        _v3 k1, k2, k3, k4;
     };
 
-    inline rk4_coefs3 _get_rk4_coefs3(const dfunc3_t &fn, double t, double dt, const v3 &x)
+    template < typename _v3 = v3 < > >
+    inline rk4_coefs3 < _v3 > _get_rk4_coefs3(const dfunc3_t < _v3 > &fn, double t, double dt, const _v3 &x)
     {
-        rk4_coefs3 c;
+        rk4_coefs3 < _v3 > c;
         c.k1 = fn(t, x) * dt;
         c.k2 = fn(t + dt / 2, x + c.k1 / 2) * dt;
         c.k3 = fn(t + dt / 2, x + c.k2 / 2) * dt;
@@ -38,9 +44,10 @@ namespace math
         return c;
     }
 
-    inline rk4_coefs3 _get_rk4_coefs3s(const dfunc3s_t &fn, double t, double dt, const v3 &x, const v3 &dx)
+    template < typename _v3 = v3 < > >
+    inline rk4_coefs3 < _v3 > _get_rk4_coefs3s(const dfunc3s_t < _v3 > &fn, double t, double dt, const _v3 &x, const _v3 &dx)
     {
-        rk4_coefs3 c;
+        rk4_coefs3 < _v3 > c;
         c.k1 = fn(t, x, dx) * dt;
         c.k2 = fn(t + dt / 2, x + dx * dt / 2, dx + c.k1 / 2) * dt;
         c.k3 = fn(t + dt / 2, x + dx * dt / 2 + c.k1 / 4 * dt, dx + c.k2 / 2) * dt;
@@ -55,9 +62,10 @@ namespace math
     // dt - the scalar parameter step (time delta)
     // x  - the initial condition
     // dx - the initial condition for the derivative
-    inline dresult3s rk4_solve3s(const dfunc3s_t & fn, double t, double dt, const v3 &x, const v3 &dx)
+    template < typename _v3 = v3 < > >
+    inline dresult3s < _v3 > rk4_solve3s(const dfunc3s_t < _v3 > & fn, double t, double dt, const _v3 &x, const _v3 &dx)
     {
-        rk4_coefs3 c = _get_rk4_coefs3s(fn, t, dt, x, dx);
+        rk4_coefs3 < _v3 > c = _get_rk4_coefs3s < _v3 > (fn, t, dt, x, dx);
         return
         {
             t + dt,
@@ -72,9 +80,10 @@ namespace math
     // t  - the scalar parameter (time)
     // dt - the scalar parameter step (time delta)
     // x  - the initial condition
-    inline dresult3 rk4_solve3(const dfunc3_t & fn, double t, double dt, const v3 &x)
+    template < typename _v3 = v3 < > >
+    inline dresult3 < _v3 > rk4_solve3(const dfunc3_t < _v3 > & fn, double t, double dt, const _v3 &x)
     {
-        rk4_coefs3 c = _get_rk4_coefs3(fn, t, dt, x);
+        rk4_coefs3 < _v3 > c = _get_rk4_coefs3 < _v3 > (fn, t, dt, x);
         return
         {
             t + dt,
@@ -90,11 +99,12 @@ namespace math
     // dt - the scalar parameter step (time delta), always positive
     // x  - the initial condition
     // note: t1 may be greater than t2, but dt must always be positive
-    inline dresult3 rk4_solve3i(const dfunc3_t & fn, double t1, double t2, double dt, const v3 &x)
+    template < typename _v3 = v3 < > >
+    inline dresult3 < _v3 > rk4_solve3i(const dfunc3_t < _v3 > & fn, double t1, double t2, double dt, const _v3 &x)
     {
         size_t iters = (size_t)(abs(t1 - t2) / dt);
         if (t1 > t2) { dt = -dt; }
-        dresult3 r = { t1, x };
+        dresult3 < _v3 > r = { t1, x };
         for (size_t i = 0; i < iters; ++i)
         {
             r = rk4_solve3(fn, r.t, dt, r.x);
@@ -112,11 +122,12 @@ namespace math
     // x  - the initial condition
     // dx - the initial condition for the derivative
     // note: t1 may be greater than t2, but dt must always be positive
-    inline dresult3s rk4_solve3si(const dfunc3s_t & fn, double t1, double t2, double dt, const v3 &x, const v3 &dx)
+    template < typename _v3 = v3 < > >
+    inline dresult3s < _v3 > rk4_solve3si(const dfunc3s_t < _v3 > & fn, double t1, double t2, double dt, const _v3 &x, const _v3 &dx)
     {
         size_t iters = (size_t)(abs(t1 - t2) / dt);
         if (t1 > t2) { dt = -dt; }
-        dresult3s r = { t1, x, dx };
+        dresult3s < _v3 > r = { t1, x, dx };
         for (size_t i = 0; i < iters; ++i)
         {
             r = rk4_solve3s(fn, r.t, dt, r.x, r.dx);
