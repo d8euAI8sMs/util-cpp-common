f5a615396bd5eb0b17cdcd023724c4d603f12502
kalaider <a.kalaider@yandex.ru>
2017-10-19 17:09:31 +0300

    Scalar arithmetic types and operations added

---
+++ include/util/common/math/scalar.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include <cmath>
+
+namespace math
+{
+
+    /*****************************************************/
+    /*                     scalar                        */
+    /*****************************************************/
+
+    template < typename _data_t >
+    inline double norm(const _data_t & d)
+    {
+    	return std::abs(d);
+    }
+
+    template < typename _data_t >
+    inline double sqnorm(const _data_t & d)
+    {
+    	return norm(d) * norm(d);
+    }
+
+    template < typename _data_t >
+    inline _data_t conjugate(const _data_t & d)
+    {
+    	return d;
+    }
+
+    /*****************************************************/
+    /*                     complex                       */
+    /*****************************************************/
+
+    template < typename _data_t = double >
+    struct complex
+    {
+    	_data_t re, im;
+
+        template < typename _second_t >
+        complex(complex < _second_t > other)
+            : re(static_cast < _data_t > (other.re))
+            , im(static_cast < _data_t > (other.im))
+        {
+        }
+
+        template < typename _second_t >
+        complex(_second_t re, _second_t im = {})
+            : re(static_cast < _data_t > (re))
+            , im(static_cast < _data_t > (im))
+        {
+        }
+
+        complex()
+            : re({}), im({})
+        {
+        }
+    };
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator+(const complex < _data_t > &first, const complex < _second_t > &second)
+    {
+    	return{ first.re + second.re, first.im + second.im };
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator-(const complex < _data_t > &first, const complex < _second_t > &second)
+    {
+    	return{ first.re - second.re, first.im - second.im };
+    }
+
+    template < typename _data_t >
+    inline complex < _data_t > operator-(const complex < _data_t > &first)
+    {
+    	return{ -first.re, -first.im };
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator*(const complex < _data_t > &first, _second_t n)
+    {
+    	return{ n * first.re, n * first.im };
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator*(_second_t n, const complex < _data_t > &first)
+    {
+    	return first * n;
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator*(const complex < _data_t > &first, const complex < _second_t > &second)
+    {
+        return{
+            first.re * second.re - first.im * second.im,
+            first.re * second.im + first.im * second.re
+        };
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator/(const complex < _data_t > &first, _second_t n)
+    {
+    	return{ first.re / n, first.im / n };
+    }
+
+    template < typename _data_t >
+    inline double norm(const complex < _data_t > &first)
+    {
+    	return std::sqrt(norm(first.re * first.re + first.im * first.im));
+    }
+
+    template < typename _data_t >
+    inline double sqnorm(const complex < _data_t > &first)
+    {
+    	return norm(first.re * first.re + first.im * first.im);
+    }
+
+    template < typename _data_t >
+    inline complex < _data_t > conjugate(const complex < _data_t > &first)
+    {
+        return { first.re, - first.im };
+    }
+
+    template < typename _data_t, typename _second_t >
+    inline complex < _data_t > operator/(const complex < _data_t > &first, const complex < _second_t > &second)
+    {
+    	return first * conjugate(second) / norm(second) / norm(second);
+    }
+}
