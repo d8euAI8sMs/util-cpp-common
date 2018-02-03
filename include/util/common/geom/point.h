c901369b9c26b335ad120a799af88f40420cac21
kalaider <a.kalaider@yandex.ru>
2018-02-03 14:49:29 +0300

    build: move point type to geom package

---
+++ include/util/common/geom/point.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <afxwin.h>
+
+#include <util/common/ptr.h>
+
+namespace plot
+{
+
+    /*****************************************************/
+    /*                     point                         */
+    /*****************************************************/
+
+    template
+    <
+        typename N
+    >
+    struct point
+    {
+        using ptr_t = util::ptr_t < point > ;
+
+        template < class ... T > static ptr_t create(T && ... t) { return util::create < typename ptr_t::element_type > (std::forward < T > (t) ...); }
+
+        using value_type = N;
+
+        N x, y;
+
+        point(N x, N y)
+            : x(x), y(y)
+        {
+        }
+
+        point()
+            : point(0, 0)
+        {
+        }
+
+        operator POINT () const
+        {
+            return{ x, y };
+        }
+
+        bool empty() const
+        {
+            return ((x == 0) && (y == 0));
+        }
+    };
+}
--- include/util/common/plot/shape.h
+++ include/util/common/plot/shape.h
@@ -3,48 +3,10 @@
 #include <afxwin.h>
 
 #include <util/common/ptr.h>
+#include <util/common/geom/point.h>
 
 namespace plot
 {
-
-    /*****************************************************/
-    /*                     point                         */
-    /*****************************************************/
-
-    template
-    <
-        typename N
-    >
-    struct point
-    {
-        using ptr_t = util::ptr_t < point > ;
-
-        template < class ... T > static ptr_t create(T && ... t) { return util::create < typename ptr_t::element_type > (std::forward < T > (t) ...); }
-
-        using value_type = N;
-
-        N x, y;
-
-        point(N x, N y)
-            : x(x), y(y)
-        {
-        }
-
-        point()
-            : point(0, 0)
-        {
-        }
-
-        operator POINT () const
-        {
-            return{ x, y };
-        }
-
-        bool empty() const
-        {
-            return ((x == 0) && (y == 0));
-        }
-    };
 
     /*****************************************************/
     /*                     rect                          */
