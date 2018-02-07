19c6d679017591dd029b45fd62f7c2462b3f9a82
kalaider <a.kalaider@yandex.ru>
2018-02-07 14:08:55 +0300

    feat(plot): add multilist drawable to plot.h

--- include/util/common/plot/plot.h
+++ include/util/common/plot/plot.h
@@ -3,6 +3,7 @@
 #include <util/common/plot/custom_drawable.h>
 #include <util/common/plot/layer_drawable.h>
 #include <util/common/plot/list_drawable.h>
+#include <util/common/plot/multilist_drawable.h>
 #include <util/common/plot/tick_drawable.h>
 #include <util/common/plot/viewporter.h>
 #include <util/common/plot/utility.h>
