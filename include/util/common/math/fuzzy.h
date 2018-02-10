124fdaf6f44135b1eb1f71cbeb025acac539499c
kalaider <a.kalaider@yandex.ru>
2018-02-11 00:00:45 +0300

    feat(math): add custom tolerance value to fuzzy

--- include/util/common/math/fuzzy.h
+++ include/util/common/math/fuzzy.h
@@ -41,32 +41,32 @@ namespace math
         using traits = _Traits;
         using type   = typename traits::type;
 
-        static int equals(type x1, type x2)
+        static int equals(type x1, type x2, type tolerance = traits::tolerance())
         {
             if (x1 == x2) return _sure;
-            if ((x1 <= x2 + traits::tolerance())
-                && (x2 <= x1 + traits::tolerance())) return _unsure;
+            if ((x1 <= x2 + tolerance)
+                && (x2 <= x1 + tolerance)) return _unsure;
             return _sure_not;
         }
 
-        static int less(type x1, type x2)
+        static int less(type x1, type x2, type tolerance = traits::tolerance())
         {
-            if (x2 > x1 + traits::tolerance()) return _sure;
-            if (x1 > x2 + traits::tolerance()) return _sure_not;
+            if (x2 > x1 + tolerance) return _sure;
+            if (x1 > x2 + tolerance) return _sure_not;
             return _unsure;
         }
 
-        static int greater(type x1, type x2)
+        static int greater(type x1, type x2, type tolerance = traits::tolerance())
         {
-            if (x2 > x1 + traits::tolerance()) return _sure_not;
-            if (x1 > x2 + traits::tolerance()) return _sure;
+            if (x2 > x1 + tolerance) return _sure_not;
+            if (x1 > x2 + tolerance) return _sure;
             return _unsure;
         }
 
-        static int compare(type x1, type x2)
+        static int compare(type x1, type x2, type tolerance = traits::tolerance())
         {
-            if (x2 > x1 + traits::tolerance()) return _less;
-            if (x1 > x2 + traits::tolerance()) return _greater;
+            if (x2 > x1 + tolerance) return _less;
+            if (x1 > x2 + tolerance) return _greater;
             return _equal;
         }
     };
